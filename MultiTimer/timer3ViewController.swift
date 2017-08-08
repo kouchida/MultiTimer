b0d990435eee82c109149683137dcce62813e539
kouchida <30708451+kouchida@users.noreply.github.com>
2017-08-08 15:27:41 +0900

    Started UI and made Views

---
+++ MultiTimer/timer3ViewController.swift
@@ -0,0 +1,9 @@
+//
+//  timer3ViewController.swift
+//  MultiTimer
+//
+//  Created by Ko Uchida on 08/08/2017.
+//  Copyright © 2017 Ko Uchida. All rights reserved.
+//
+
+import Foundation
---
+++ MultiTimer/timerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  timerViewController.swift
+//  MultiTimer
+//
+//  Created by Ko Uchida on 08/08/2017.
+//  Copyright © 2017 Ko Uchida. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var seconds = 30
+    var timer = Timer()
+    var audioPlayer = AVAudioPlayer()
+
+
+
+
+}
